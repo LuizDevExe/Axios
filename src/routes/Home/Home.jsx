d2c038434ede07591cf22524f05fd4d3032931f0
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2023-10-28 18:16:59 -0300

    Estruturação e estilização das rotas home e NewPost

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
+import './home.css'
+import blogFetch from '../../axios/config'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async() => {
+    
+    try{
+      const response = await blogFetch.get(
+        "/posts"
+      );
+
+      const data = response.data;
+
+        setPosts(data);
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+  }
+
+  useEffect(() => {
+
+    getPosts();
+
+
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>últimos posts</h1>
+      {
+        posts.length === 0 ? (<p>Carregnado...</p>) : (
+          posts.map((post) =>(
+            <div className='post' key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <Link to={`/posts/${post.id}`} className='btn'>Ler mais</Link>
+            </div>
+          ))
+        )
+      }
+    </div>
   )
 }
 
--- src/routes/NewPost/NewPost.jsx
+++ src/routes/NewPost/NewPost.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className='new-post'>
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            id='title'
+            placeholder='Digite o título'
+          />
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea 
+          name="body" 
+          id="body" 
+          placeholder='DIgite o conteúdo'
+          ></textarea>
+          <input type="submit" value="Criar post" className="btn" id='submit'/>
+        </div>
+      </form>
+    </div>
   )
 }
 
